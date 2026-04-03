441b543a8630b4fa0e56cfee76493927ad683fca
Bombella <nmasionyena@gmail.com>
2026-04-03 01:41:41 +0100

    Introduction

--- Greetings.java
+++ Greetings.java
@@ -1,9 +1,10 @@
 public class Greetings{
 	public static void main(String[]args){
-}
+
 		System.out.println("Hello Everyone, my name is Bombella and I am here to eat lots of snacks");
 
 
 
 }
 
+}
